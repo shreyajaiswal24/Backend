bd7c2b283b91b01cb6d7759ecdf3008476b68d31
shreya jaiswal <130988801+shreyajaiswal24@users.noreply.github.com>
2025-02-05 09:56:21 +0530

    new file2

--- CE/file.js
+++ CE/file.js
@@ -1,5 +1,12 @@
 const fs= require('fs')
-// const data = fs.readFileSync("./data.txt","utf-8");
-// console.log(data);
-fs.appendFileSync("data1.txt","AKTU");
-fs.renameSync("./data1.txt", "./data2.txt")
+const data = fs.readFileSync("./data.txt","utf-8");
+console.log(data);
+// fs.appendFileSync("data1.txt","AKTU");
+// // fs.renameSync("./data1.txt", "./data2.txt")
+// fs.unlinkSync("./data2.txt");
+
+if(data.match("h")){
+    console.log("h is present");
+   const changedata= data.replace("h", "ABES");
+   fs.writeFileSync("./data.txt",changedata);
+}

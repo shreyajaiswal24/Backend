f8f2d4e84992af5dc5cac51f84cf6cdb5afd6a2b
shreya jaiswal <130988801+shreyajaiswal24@users.noreply.github.com>
2025-02-05 09:46:28 +0530

    new file

--- CE/file.js
+++ CE/file.js
@@ -1,3 +1,5 @@
 const fs= require('fs')
-const data = fs.readFileSync("./data.txt","utf-8");
-console.log(data);
+// const data = fs.readFileSync("./data.txt","utf-8");
+// console.log(data);
+fs.appendFileSync("data1.txt","AKTU");
+fs.renameSync("./data1.txt", "./data2.txt")
